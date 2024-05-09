53686d43a44d937db6df16f068470f37c90b4984
Navin Avadhani <knavinavadhani@gmail.com>
2024-05-10 01:05:05 +0530

    chore: Removed apps/docs

--- apps/fondisplit/app/page.tsx
+++ apps/fondisplit/app/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
 export default function Page(): JSX.Element {
-  return <main>Hello World</main>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      Hello World
+    </main>
+  );
 }
