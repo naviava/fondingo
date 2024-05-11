ade1e734b2043a16c64c9f4b72ee8dda3967192d
Navin Avadhani <knavinavadhani@gmail.com>
2024-05-11 18:02:55 +0530

    chore: Added NextJSTopLoader

--- apps/fondisplit/app/layout.tsx
+++ apps/fondisplit/app/layout.tsx
@@ -2,7 +2,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 
+import NextTopLoader from "nextjs-toploader";
 import { getServerSession } from "next-auth";
+
 import { cn } from "@fondingo/ui/utils";
 import { authOptions } from "~/lib/auth";
 
@@ -26,6 +28,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={cn("antialiased", font.className)}>
+        <NextTopLoader />
         <SessionProvider session={session}>
           <Providers>
             <main className="h-full bg-black/20">{children}</main>
