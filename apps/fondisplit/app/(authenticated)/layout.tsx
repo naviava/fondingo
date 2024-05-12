67d9a0a1cf76d90e8ea0102c09555f9e461e0948
Navin Avadhani <knavinavadhani@gmail.com>
2024-05-12 18:30:44 +0530

    feat: Add server-side authentication check to AuthenticatedLayout

--- apps/fondisplit/app/(authenticated)/layout.tsx
+++ apps/fondisplit/app/(authenticated)/layout.tsx
@@ -1,10 +1,20 @@
+import { authOptions } from "~/lib/auth";
+import { getServerSession } from "next-auth";
+
 import { Navbar } from "~/components/navbar";
+import { redirect } from "next/navigation";
 
 interface IProps {
   children: React.ReactNode;
 }
 
-export default function AuthenticatedLayout({ children }: IProps) {
+export default async function AuthenticatedLayout({ children }: IProps) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return redirect("/api/auth/signin");
+  }
+
   return (
     <div className="relative mx-auto flex h-dvh w-full max-w-xl flex-col bg-[#F4F4F4]">
       {children}
