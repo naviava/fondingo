e19d2ab9e0ab66d2fa8b27fabff7980c9609e263
Navin Avadhani <knavinavadhani@gmail.com>
2024-05-31 04:01:02 +0530

    refactor: Removed @fondingo/db-auth, @fondingo/auth and related files

--- apps/auth/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@fondingo/ui/button";
-
-export default function Home() {
-  return (
-    <main className="flex flex-grow items-center justify-center">
-      <Button className="text-4xl text-blue-500">Hello monorepo</Button>
-    </main>
-  );
-}
