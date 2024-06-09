d3e419d0daeb70f8326fdb656b4f495e6ed5c3cb
Navin Avadhani <knavinavadhani@gmail.com>
2024-06-09 18:58:43 +0530

    feat: Added MainBanner on landing page

--- apps/fsplit/components/landing-page/main-banner.tsx
+++ apps/fsplit/components/landing-page/main-banner.tsx
@@ -1,4 +1,9 @@
+import { cn } from "@fondingo/ui/utils";
 import Image from "next/image";
+import { archivo } from "~/utils";
+import { FlipWords } from "@fondingo/ui/flip-words";
+
+const WORDS = ["Effortless", "Simple", "Secure", "Reliable", "Convenient"];
 
 interface IProps {}
 
@@ -13,7 +18,28 @@ export function MainBanner({}: IProps) {
           className="object-cover"
         />
       </div>
-      <div className=" flex-1">A</div>
+      <div className="flex-1 space-y-10">
+        <h1
+          className={cn(
+            "lg:leading-tighter mx-auto text-center text-3xl font-bold tracking-tighter sm:text-4xl md:max-w-[70%] md:text-left md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]",
+            archivo.className,
+          )}
+        >
+          <FlipWords words={WORDS} className="text-cta" />
+          <br />
+          <div className="pl-1">
+            Group
+            <br />
+            Expense
+            <br />
+            Management
+          </div>
+        </h1>
+        <p className="mx-auto max-w-[70%] pl-1 text-center text-gray-500 md:text-left md:text-xl">
+          FSplit makes it easy to track expenses, split bills, and get paid back
+          by your friends and family.
+        </p>
+      </div>
     </div>
   );
 }
