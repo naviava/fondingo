bb59a32585c8afbc4dcb0517aaa0cd308111483e
Navin Avadhani <knavinavadhani@gmail.com>
2024-06-10 15:48:53 +0530

    fix: Fixed CTA Banner animation

--- apps/fsplit/components/landing-page/header-animation.tsx
+++ apps/fsplit/components/landing-page/header-animation.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion, useAnimate } from "framer-motion";
 
 export function HeaderAnimation() {
-  const [toggleState, setToggleState] = useState(false);
+  const [scope, animate] = useAnimate();
+  const [toggleState, setToggleState] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -13,23 +14,51 @@ export function HeaderAnimation() {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    async function handleAnimation() {
+      if (toggleState) {
+        const animations = [
+          animate(
+            "#bg",
+            { scaleX: 1, backgroundColor: "#11998E" },
+            { duration: 1 },
+          ),
+          animate("#text", { color: "#fff" }, { duration: 1.5 }),
+        ];
+        await Promise.all(animations);
+        // @ts-expect-error
+        await animate("#bg", { originY: 1 });
+      }
+      if (!toggleState) {
+        const animations = [
+          animate(
+            "#bg",
+            { scaleY: 0, backgroundColor: "#10b981" },
+            { duration: 1 },
+          ),
+          animate("#text", { color: "#404040" }, { duration: 1 }),
+        ];
+        await Promise.all(animations);
+        await animate("#bg", { scaleX: 0, backgroundColor: "#10b981" });
+        await animate("#bg", { scaleY: 1 });
+      }
+    }
+    handleAnimation();
+  }, [toggleState, animate, scope]);
+
   return (
     <h2 className="text-balance text-4xl font-bold leading-[1.5em]  md:flex-row md:text-5xl md:leading-[1.5em] lg:text-6xl lg:leading-[1.5em]">
       <div className="flex flex-col items-center gap-y-2 md:gap-y-4">
         <div>Start Managing Your Expenses</div>
-        <div className="relative mx-auto w-fit">
-          <AnimatePresence mode="sync">
-            {toggleState && (
-              <motion.div
-                initial={{ scaleX: 0, originX: 0 }}
-                animate={{ scaleX: 1 }}
-                exit={{ scaleX: 0, originX: 1 }}
-                transition={{ duration: 1 }}
-                className="absolute inset-0 rounded-lg bg-emerald-500"
-              />
-            )}
-          </AnimatePresence>
-          <div className="relative px-2">Today</div>
+        <div ref={scope} className="relative mx-auto w-fit">
+          <motion.div
+            id="bg"
+            initial={{ scaleX: 0, originX: 0, backgroundColor: "#10b981" }}
+            className="absolute inset-0 rounded-lg"
+          />
+          <div id="text" className="relative px-2">
+            Today
+          </div>
         </div>
       </div>
     </h2>
