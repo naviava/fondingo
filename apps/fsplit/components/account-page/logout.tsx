92151cf2338ab43d0ad45f433b389b1daab3c8c6
Navin Avadhani <knavinavadhani@gmail.com>
2024-06-11 12:12:14 +0530

    ui: Added logo on account page

--- apps/fsplit/components/account-page/logout.tsx
+++ apps/fsplit/components/account-page/logout.tsx
@@ -7,27 +7,36 @@ import { signOut } from "next-auth/react";
 import { cn } from "@fondingo/ui/utils";
 import { hfont } from "~/utils";
 import { Loader } from "@fondingo/ui/lucide";
+import { Logo } from "../logo";
 
 export function Logout() {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <div className="my-6">
-      <Button
-        size="lg"
-        variant="ctaGhost"
-        disabled={isLoading}
-        onClick={() => {
-          setIsLoading(true);
-          signOut({ callbackUrl: "/" });
-        }}
-        className={cn(
-          "w-full rounded-none py-8 hover:bg-neutral-200",
-          hfont.className,
-        )}
-      >
-        {isLoading ? <Loader className="h-6 w-6 animate-spin" /> : "Log out"}
-      </Button>
-    </div>
+    <>
+      <div className="my-6">
+        <Button
+          size="lg"
+          variant="ctaGhost"
+          disabled={isLoading}
+          onClick={() => {
+            setIsLoading(true);
+            signOut({ callbackUrl: "/" });
+          }}
+          className={cn(
+            "w-full rounded-none py-8 hover:bg-neutral-200",
+            hfont.className,
+          )}
+        >
+          {isLoading ? <Loader className="h-6 w-6 animate-spin" /> : "Log out"}
+        </Button>
+      </div>
+      <div className="flex flex-col items-center pb-16">
+        <Logo />
+        <small className="mx-auto mt-6 text-neutral-400">
+          © 2024 Fondingo. All rights reserved.
+        </small>
+      </div>
+    </>
   );
 }
